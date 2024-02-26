d657dd8054856bcbfcda82370a9c6ba33079f380
karthikrajan4 <kkartik64@gmail.com>
2024-02-27 04:55:39 +0530

    weekly push

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"6daed34b-d3a9-4be2-a9c1-627b7405ec4c","feature":"To check the full flow functionality of SwagLabs order placement","scenario":"SC01","start":1705215244421,"group":35,"content":"","tags":"@swaglabfullflow,","end":1705215253771,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"93eadebc-4725-4451-8480-c1b10552bfab","feature":"To check the full flow functionality of SwagLabs order placement","scenario":"SC01","start":1708988975835,"group":35,"content":"","tags":"@swaglabfullflow,","end":1708988987407,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":35,"content":"Thread[#35,TestNG-tests-1,5,main]"}]);
 });
